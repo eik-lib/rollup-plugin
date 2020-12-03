121c626ca1598857132f25e58195ede73b761478
Trygve Lie <trygve.lie@finn.no>
2020-12-03 12:55:44 +0100

    chore: Fix build and expose both ESM and CJS modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
 export default {
     input: 'src/plugin.js',
-    external: ['node-fetch', 'url'],
+    external: [
+        'rollup-plugin-import-map',
+        'node-fetch', 
+        'path',
+        'url',
+        'fs'
+    ],
     output: [
-        { file: 'dist/plugin.cjs.js', format: 'cjs' },
+        { file: 'dist/plugin.cjs', format: 'cjs' },
     ],
 };
