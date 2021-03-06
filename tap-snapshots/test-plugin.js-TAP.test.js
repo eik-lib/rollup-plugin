6dac5536ad79d992de2ab27186d00666bf18d5de
Trygve Lie <trygve.lie@finn.no>
2021-03-06 15:59:03 +0100

    fix: Align precedence of maps with other plugins

--- tap-snapshots/test-plugin.js-TAP.test.js
+++ tap-snapshots/test-plugin.js-TAP.test.js
@@ -5,10 +5,67 @@
  * Make sure to inspect the output below.  Do not ignore changes!
  */
 'use strict'
+exports[`test/plugin.js TAP plugin() - Import map defined through constructor "maps" argument take precedence over import map defined in eik.json > Should rewrite import statement to https://cdn.eik.dev/lit-element/v2 1`] = `
+import { html } from 'lit-html/lit-html';
+import { css } from 'lit-html';
+import { LitElement } from 'https://cdn.eik.dev/lit-element/v2';
+
+class Inner extends LitElement {
+    static get styles() {
+        return [css\`:host { color: red; }\`];
+    }
+
+    render(world) {
+        return html\`<p>Hello \${world}!</p>\`;
+    }
+}
+
+export default Inner;
+
+`
+
+exports[`test/plugin.js TAP plugin() - Import map defined through constructor "maps" argument take precedence over import map defined through constructor "urls" argument > Should rewrite import statement to https://cdn.eik.dev/lit-element/v2 1`] = `
+import { html } from 'lit-html/lit-html';
+import { css } from 'lit-html';
+import { LitElement } from 'https://cdn.eik.dev/lit-element/v2';
+
+class Inner extends LitElement {
+    static get styles() {
+        return [css\`:host { color: red; }\`];
+    }
+
+    render(world) {
+        return html\`<p>Hello \${world}!</p>\`;
+    }
+}
+
+export default Inner;
+
+`
+
+exports[`test/plugin.js TAP plugin() - Import map defined through constructor "urls" argument take precedence over import map defined in eik.json > Should rewrite import statement to https://cdn.eik.dev/lit-element/v2 1`] = `
+import { html } from 'lit-html/lit-html';
+import { css } from 'lit-html';
+import { LitElement } from 'https://cdn.eik.dev/lit-element/v2';
+
+class Inner extends LitElement {
+    static get styles() {
+        return [css\`:host { color: red; }\`];
+    }
+
+    render(world) {
+        return html\`<p>Hello \${world}!</p>\`;
+    }
+}
+
+export default Inner;
+
+`
+
 exports[`test/plugin.js TAP plugin() - import map fetched from a URL > import maps from urls 1`] = `
-import { html } from 'https://cdn.pika.dev/lit-html/v2';
-import { css } from 'https://cdn.pika.dev/lit-html/v1';
-import { LitElement } from 'https://cdn.pika.dev/lit-element/v2';
+import { html } from 'https://cdn.eik.dev/lit-html/v2';
+import { css } from 'https://cdn.eik.dev/lit-html/v1';
+import { LitElement } from 'https://cdn.eik.dev/lit-element/v2';
 
 class Inner extends LitElement {
     static get styles() {
@@ -25,28 +82,9 @@ export default Inner;
 `
 
 exports[`test/plugin.js TAP plugin() - import map fetched from a URL via eik.json > eik.json import-map string 1`] = `
-import { html } from 'https://cdn.pika.dev/lit-html/v2';
-import { css } from 'https://cdn.pika.dev/lit-html/v1';
-import { LitElement } from 'https://cdn.pika.dev/lit-element/v2';
-
-class Inner extends LitElement {
-    static get styles() {
-        return [css\`:host { color: red; }\`];
-    }
-
-    render(world) {
-        return html\`<p>Hello \${world}!</p>\`;
-    }
-}
-
-export default Inner;
-
-`
-
-exports[`test/plugin.js TAP plugin() - import maps via eik.json, URLs and direct definitions > import maps from eik.json, urls and direct definition 1`] = `
-import { html } from 'https://cdn.pika.dev/lit-html/v2';
-import { css } from 'https://cdn.pika.dev/lit-html/v1';
-import { LitElement } from 'https://cdn.pika.dev/lit-element/v2';
+import { html } from 'https://cdn.eik.dev/lit-html/v2';
+import { css } from 'https://cdn.eik.dev/lit-html/v1';
+import { LitElement } from 'https://cdn.eik.dev/lit-element/v2';
 
 class Inner extends LitElement {
     static get styles() {
